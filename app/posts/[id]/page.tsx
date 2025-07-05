f0f0fa5a90aca842bbe55c5c7a67940ea48f0121
Granit Halimi <halimigranit6@gmail.com>
2025-07-05 23:41:54 +0200

    ts changes 2

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -2,13 +2,7 @@ import { Suspense } from "react";
 import PostSkeleton from "@/app/skeletons/PostSkeleton";
 import Post from "@/app/components/post";
 
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
-
-export default async function Page({ params }:PageProps) {
+export default async function Page({ params }: any) {
   const { id } = await params;
   return (
     <div>
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import {Montserrat, Roboto, Inter, Poppins} from "next/font/google";
+import {Montserrat, Roboto, Poppins} from "next/font/google";
 
 export const montserrat = Montserrat({subsets: ["latin"], weight:["700"]});
 export const roboto = Roboto({subsets: ["latin"]});
